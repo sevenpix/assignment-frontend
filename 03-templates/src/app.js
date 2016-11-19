d1e4bd5f17b456e0d0c6050c00e5372178cc92b3
Mario Taferner <mario.taferner@gmail.com>
2016-11-19 13:42:34 +0100

    Added exercise 03

--- 03-templates/src/app.js
+++ 03-templates/src/app.js
@@ -1,12 +1,24 @@
 import $ from 'jquery'
 import router from './router'
 import homeTpl from './templates/home.hbs'
-import magnusTpl from './templates/magnus.hbs'
-import sergeyTpl from './templates/sergey.hbs'
+import playersTpl from './templates/players.hbs'
 import contactTpl from './templates/contact.hbs'
 import notFoundTpl from './templates/not-found.hbs'
 
 const $app = $('#app')
+
+const playerDetails = {
+  'magnus': {
+    name: 'Magnus Carlsen',
+    image: 'https://cdn.worldchess.com/static/img/nyfide/carlsen_2x.png',
+    description: 'Carlsen is a former chess prodigy. He became a Grandmaster in 2004, at the age of 13 years, 148 days. This made him the third-youngest grandmaster in history. In November 2013 Carlsen became World Champion by defeating Viswanathan Anand in the World Chess Championship 2013. On the May 2014 FIDE rating list, Carlsen reached his peak rating of 2882, which is the highest in history. He successfully defended his title in November 2014, once again defeating Anand. The same year, he also won the World Rapid Championship and the World Blitz Championship, thus holding all three world championship titles. In 2015 Carlsen won the inaugural Grand Chess Tour, a series of three supertournaments featuring the 10 best chess grandmasters in the world.'
+  },
+  'sergey': {
+    name: 'Sergey Karjakin',
+    image: 'https://cdn.worldchess.com/static/img/nyfide/karjakin_2x.png',
+    description: 'On March 28, 2016, Sergey Karjakin became the Challenger to Magnus Carlsen in the World Chess Championship 2016 after winning the Candidates Tournament 2016 in Moscow. Karjakin won the 2012 World Rapid Chess Championship and the Chess World Cup 2015. He also won the Norway Chess Tournament twice (2013, 2014) and the Corus Chess Tournament in 2009.'
+  }
+}
 
 function index() {
   $app.html(homeTpl())
@@ -17,16 +29,8 @@ function contact() {
 }
 
 function players(ctx) {
-  let tpl = () => {}
-  switch (ctx.params.player) {
-    case 'magnus':
-      tpl = magnusTpl
-      break;
-    case 'sergey':
-      tpl = sergeyTpl
-      break;
-  }
-  $app.html(tpl())
+  let player = playerDetails[ctx.params.player]
+  $app.html(playersTpl(player))
 }
 
 function notFound() {
